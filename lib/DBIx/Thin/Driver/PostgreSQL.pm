614b3ab841ab24518d4533008cd5f934571b5742
oinume <oinume@gmail.com>
2009-09-14 01:59:54 +0900

    Add drivers

---
+++ lib/DBIx/Thin/Driver/PostgreSQL.pm
@@ -0,0 +1,17 @@
+package DBIx::Thin::Driver::PostgreSQL;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Thin::Driver/;
+
+sub last_insert_id {
+    my ($self, $sth, $opts) = @_;
+    return $self->_dbh->last_insert_id(undef, undef, $opts->{table}, undef);
+}
+
+sub sql_for_unixtime {
+    return "TRUNC(EXTRACT('epoch' from NOW()))";
+}
+
+1;
---
+++ lib/DBIx/Thin/Driver/SQLite.pm
@@ -0,0 +1,13 @@
+package DBIx::Thin::Driver::SQLite;
+
+use strict;
+use warnings;
+
+use base qw/DBIx::Thin::Driver/;
+
+sub last_insert_id {
+    my ($self, $sth, $opts) = @_;
+    return $self->_dbh->func('last_insert_rowid');
+}
+
+1;
