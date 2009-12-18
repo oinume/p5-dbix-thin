81693cfa1f33c62413478f2e266ef2d413b5faa5
oinume <oinume@gmail.com>
2009-12-18 23:28:24 +0900

    updated comment

--- t/lib/Your/Model/Inflate.pm
+++ t/lib/Your/Model/Inflate.pm
@@ -7,12 +7,12 @@ use DBIx::Thin::Inflate;
 register_inflate dt => {
     inflate => sub {
         my ($column, $value) = @_;
-        $value =~ s!-!/!g; # YYYY-MM-DD hh:mm:ss -> YYYY/MM/DD hh:mm::ss
+        $value =~ s!-!/!g; # YYYY-MM-DD hh:mm:ss -> YYYY/MM/DD hh:mm:ss
         return $value;
     },
     deflate => sub {
         my ($column, $value) = @_;
-        $value =~ s!/!-!g; # YYYY/MM/DD hh:mm:ss -> YYYY-MM-DD hh:mm::ss
+        $value =~ s!/!-!g; # YYYY/MM/DD hh:mm:ss -> YYYY-MM-DD hh:mm:ss
         return $value;
     },
 };
